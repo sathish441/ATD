c52bf0f4bc92d81083fb9b58f693c9a529c2998c
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-11-22 13:07:35 +0530

    zgdru

---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/AtdMultiInsuranceServicesApplication.java
@@ -0,0 +1,20 @@
+package com.insurance.ATD;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@SpringBootApplication
+public class AtdMultiInsuranceServicesApplication extends SpringBootServletInitializer {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AtdMultiInsuranceServicesApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtdMultiInsuranceServicesApplication.class, args);
+	}
+
+}
