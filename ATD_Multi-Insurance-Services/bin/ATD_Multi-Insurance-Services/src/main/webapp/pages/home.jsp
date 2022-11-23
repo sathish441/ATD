86b5e21eafbe66f4be2acdf69a72df7f41eb023d
DileepDuda <dileepd2.vtalent@gmail.com>
2022-11-23 14:23:58 +0530

    added

--- ATD_Multi-Insurance-Services/bin/ATD_Multi-Insurance-Services/src/main/webapp/pages/home.jsp
+++ ATD_Multi-Insurance-Services/bin/ATD_Multi-Insurance-Services/src/main/webapp/pages/home.jsp
@@ -4,14 +4,10 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Home</title>
+<title>Home Page</title>
 </head>
 <body>
-
-
-<h2>Welcome to ATD Insurance- Home for multiple Insurance Services</h2>
-
-<table align = "center">
+<table>
 <tr><td>For Fire related Stocks:</td>
 <td><button> <a href="FireInsurance">Fire Insurance</a></button></td></tr>
 <tr><td>Because Life Matters:</td>
@@ -27,24 +23,16 @@
 <tr><td>For those lovely vehicles:</td>
 <td><button>Vehicle Insurance</button></td></tr>
 <tr><td>For protecting your precious data:</td>
-<td><button>Cyber-Attack Insurance</button></td></tr>
+<button>Cyber-Attack Insurance</button>
 <tr><td>For protecting your property:</td>
-<td><button>Property Insurance</button></td></tr>
-<tr><td>For protecting your Home:</td>
-<td><button>Home Insurance</button></td></tr>
-<tr><td>For safeguarding education:</td>
-<td><button>Education Insurance</button></td></tr>
-<tr><td>For protecting your mobile:</td>
-<td><button>Mobile Insurance</button></td></tr>
-<tr><td>For protecting your gold:</td>
-<td><button>Gold Insurance</button></td></tr>
-<tr><td>For protecting your hotel:</td>
-<td><button>Hotel Insurance</button></td></tr>
-<tr><td>For protecting your crop:</td>
-<td><button>Crop Insurance</button></td></tr>
+<button>Property Insurance</button>
+<button>Home Insurance</button>
+<button>Education Insurance</button>
+<button>Mobile Insurance</button>
+<button>Gold Insurance</button>
+<button>Hotel Insurance</button>
+<button>Crop Insurance</button>
+
 </table>
-
-
-
 </body>
 </html>
---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/VehicleInsurance/VehicleBean.java
@@ -0,0 +1,92 @@
+package com.insurance.ATD.VehicleInsurance;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "VehicleInsurance")
+public class VehicleBean {
+	
+@Id
+@Column(name = "policynumber")
+private long PolicyNumber;
+
+@Column(name = "policyholder name")
+private String PolicyHolderName;
+
+@Column(name = "VehicleType")
+private String VehicleType;
+
+@Column(name = "policyamount")
+private long PolicyAmount;
+
+@Column(name = "tenure")
+private int Tenure;
+
+@Column(name = "percentage")
+private int Percentage;
+
+public long getPolicyNumber() {
+	return PolicyNumber;
+}
+
+public void setPolicyNumber(long policyNumber) {
+	this.PolicyNumber = policyNumber;
+}
+
+public String getPolicyHolderName() {
+	return PolicyHolderName;
+}
+
+public void setPolicyHolderName(String policyHolderName) {
+	this.PolicyHolderName = policyHolderName;
+}
+
+public String getVehicleType() {
+	return VehicleType;
+}
+
+public void setVehicleType(String vehicleType) {
+	this.VehicleType = vehicleType;
+}
+
+public long getPolicyAmount() {
+	return PolicyAmount;
+}
+
+public void setPolicyAmount(long policyAmount) {
+	this.PolicyAmount = policyAmount;
+}
+
+public int getTenure() {
+	return Tenure;
+}
+
+public void setTenure(int tenure) {
+	this.Tenure = tenure;
+}
+
+public int getPercentage() {
+	return Percentage;
+}
+
+public void setPercentage(int percentage) {
+	this.Percentage = percentage;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+	
+
+}
