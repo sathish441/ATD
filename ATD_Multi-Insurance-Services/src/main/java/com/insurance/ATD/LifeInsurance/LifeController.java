6d7c42d94c449db2c03a24495d43e7e727441b6a
Madhubabu <madhugovada02@gmail.com>
2022-11-24 17:17:38 +0530

    commit

--- ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/LifeInsurance/LifeController.java
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/LifeInsurance/LifeController.java
@@ -1,8 +1,16 @@
 package com.insurance.ATD.LifeInsurance;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LifeController {
+	@Autowired
 	LifeRepo lifeRepo;
+	
+	@RequestMapping(value="LifeInsurance")
+	public String LifeInsurance() {
+		return "LifeOptions";
+	}
 }
