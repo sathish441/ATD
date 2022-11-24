2b2d1ab8665b2fba027688d3ad5f5e75d24a05cf
Madhubabu <madhugovada02@gmail.com>
2022-11-24 16:10:11 +0530

    Commit

---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/LifeInsurance/LifeController.java
@@ -0,0 +1,8 @@
+package com.insurance.ATD.LifeInsurance;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LifeController {
+	LifeRepo lifeRepo;
+}
---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/LifeInsurance/LifeRepo.java
@@ -0,0 +1,7 @@
+package com.insurance.ATD.LifeInsurance;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LifeRepo extends CrudRepository<LifeBean, Integer> {
+
+}
